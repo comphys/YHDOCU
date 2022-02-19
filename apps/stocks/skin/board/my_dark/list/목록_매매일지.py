ddcc4772bc0f37d7648151e6ba01b2d0878de494
comphys <comphys@gmail.com>
2022-02-19 11:24:03 +0900

    20220219 b1

--- apps/stocks/skin/board/my_dark/list/목록_매매일지.py
+++ apps/stocks/skin/board/my_dark/list/목록_매매일지.py
@@ -45,7 +45,26 @@ class 목록_매매일지(SKIN) :
 
                 self.D['종가변동'] = f"{(c_price[-1] - c_price[0]) / c_price[0] * 100:5.2f}%"
                 self.D['평가변동'] = f"{(m_price[-1] - m_price[0]) / m_price[0] * 100:5.2f}%"
+        
+        if  self.D['bid'] in ('daily_first','daily_second','daily_third') :
+            self.DB.tbl, self.DB.wre = (self.D['tbl'],f"add1='{self.D['code']}'")
+            start_date, last_date = self.DB.get("min(add0),max(add0)",many=1,assoc=False)
 
+            self.DB.wre = f"add0='{start_date}'"
+            기본자산, 추가자산 = self.DB.get("sub7,sub8",many=1,assoc=False) 
+
+            self.DB.wre = f"add0='{last_date}'"
+            평가금액, 가용잔액, 추가자본 = self.DB.get("add11,add16,add17",many=1,assoc=False) 
+
+            초기자본 = float(기본자산) + float(추가자산)
+            최종자본 = float(평가금액) + float(가용잔액) + float(추가자본)
+            최종수익 = 최종자본 - 초기자본 
+            최종수익률 = (최종수익/초기자본) * 100 
+            style1 = "<span style='font-weight:bold;color:white'>"
+            style2 = "<span style='font-weight:bold;color:#CEF6CE'>"
+            style3 = "<span style='font-weight:bold;color:#F6CECE'>"
+            self.D['earning_info']  = f"초기자본 {style1}${초기자본:,}</span> 최종자본 {style1}${최종자본:,.2f}</span> 으로 "
+            self.D['earning_info'] += f"수익은 {style2}${최종수익:,.2f}</span> 이며 수익률은 {style3}{최종수익률:,.2f}%</span> 입니다"            
 
     def list(self) :
 
