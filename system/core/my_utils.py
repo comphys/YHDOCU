bd6fcf2a11a79e7d03bed033af913e5ac4aa2b6d
comphys <comphys@gmail.com>
2022-03-11 16:56:06 +0900

    20220311 v1

--- system/core/my_utils.py
+++ system/core/my_utils.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, json,urllib.request as ul 
 from datetime import datetime, timedelta
 from pytz import timezone
 
@@ -116,4 +116,25 @@ def dayofdate(theday,delta=0) :
     a = datetime.strptime(theday,'%Y-%m-%d')
     if delta : b = a+timedelta(days=delta) ; return (b.strftime('%Y-%m-%d'),dow[b.weekday()])
     else : return dow[a.weekday()]
+
+# stock
+
+def get_stock_data(symbol,start_date,end_date='') :
+
+    if not end_date : date_b = dayofdate(start_date,-5)[0]; date_e = start_date
+    else : date_b = start_date; date_e = end_date           
+
+    app_key = '92DC8890E1874E1D97EE34387D72F152'
+    url  = f"https://api.stockdio.com/data/financial/prices/v1/GetHistoricalPrices?app-key={app_key}&stockExchange=USA&symbol={symbol}&from={date_b}&to={date_e}"
+
+    data = ul.urlopen(url).read(json.loads(data))
+
+    col = data['data']['prices']['columns']
+    rst = data['data']['prices']['values']
+
+    rst2 = [ [x[0][:10],float(x[1]),float(x[2]),float(x[3]),float(x[4]),int(x[5]),0.0] for x in rst]
+    for i in range(1,len(rst2)) : rst2[i][6] = round((rst2[i][4] - rst2[i-1][4])/rst2[i-1][1]*100,1)
+
+    rst3 = rst2[1:] if end_date else rst2[-1]
+    return {'count':len(rst3),'column':col,'data':rst3}
 
