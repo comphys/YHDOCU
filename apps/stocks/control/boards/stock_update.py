b0e3ee2b2f01bbcde046056519fade2cebb14699
comphys <comphys@gmail.com>
2022-03-15 10:17:38 +0900

    20220315 v1

--- apps/stocks/control/boards/stock_update.py
+++ apps/stocks/control/boards/stock_update.py
@@ -38,7 +38,7 @@ class Stock_update(Control) :
         self.set_message("종목 삭제를 완료하였습니다")
         return self.moveto('board/list/stockHistory')
 
-    def update_stock(self,cdx,USER) :
+    def update_stock2(self,cdx,USER) :
 
         self.DB.tbl, self.DB.wre = ('h_stockHistory_board',f"add1='{cdx}'")
         start_b = self.DB.get("max(add0)",many=1,assoc=False) 
@@ -62,7 +62,7 @@ class Stock_update(Control) :
             sql = f"INSERT INTO {self.DB.tbl} ({db_keys}) VALUES({values})"
             self.DB.exe(sql)
 
-    def update_stock_old(self,cdx,USER) :
+    def update_stock(self,cdx,USER) :
 
         self.DB.tbl = 'h_stockHistory_board'
         self.DB.wre = f"add1='{cdx}'"
@@ -79,12 +79,14 @@ class Stock_update(Control) :
         data_index  = [x.strftime('%Y-%m-%d') for x in df.index]
         data_db_in  = list(zip(data_index, df['Close'], df['Open'], df['High'], df['Low'], df['Volume'], df['Change']))
         
-        db_keys = "add0,add3,add4,add5,add6,add7,add8,add1,add2,uid,uname,wdate,mdate"
+        db_keys = "add0,add3,add4,add5,add6,add7,add8,add9,add10,add1,add2,uid,uname,wdate,mdate"
         time_now = ut.now_timestamp()
         
         cdx = cdx.upper()
         for row in data_db_in :
             row2 = list(row)
+            row2.append(0)
+            row2.append(0)
             row2.append(cdx)
             row2.append(cdx)
             row2.append(USER['uid'])
