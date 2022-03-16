823ccd8a2353325d9597fdcc516c51625a76b0c6
comphys <comphys@gmail.com>
2022-03-17 01:19:05 +0900

    220317 v2

---
+++ mytask/myutils/DB.py
@@ -0,0 +1,171 @@
+import sqlite3
+
+class DB :
+    def __init__(self,dbname) :
+
+        self.wre = ''
+        self.odr = ''
+        self.lmt = ''
+        self.qry = ''
+        self.num = 0
+        self.tbl = ''
+        self.err = ''
+        # mydb = '/home/comphys/YHDOCU/mydb/' + dbname + '.sqlite'
+        mydb = 'C:/YHDOCU/mydb/' + dbname + '.sqlite'
+        self.con = sqlite3.connect(mydb, check_same_thread=True)
+        self.cur = self.con.cursor()
+
+    def close(self) :
+        self.con.close()
+
+    def close(self) :
+        self.con.close()
+
+    def cnt(self,qry) :
+        self.rst = self.cur.execute(qry)
+        return len(self.rst.fetchall())
+    
+    def one(self,qry) : 
+        self.rst = self.cur.execute(qry)
+        return self.rst.fetchone()[0]
+
+    def get_one(self,fld) :
+        return self.get(fld,many=1,assoc=False)
+
+    def get_line(self,fld) :
+        return self.get(fld,many=1,assoc=True)    
+    
+    def get(self,fld,many=0,assoc=True) :
+        if not self.tbl : 
+            self.err = "No table name is specified"
+            return False
+        if fld == '' : 
+            self.err = "No fields are specified"
+            return False
+
+        fld = ','.join(fld) if type(fld) is list else fld
+
+        wre = "WHERE "      + self.wre if self.wre else ''
+        lmt = "LIMIT "      + self.lmt if self.lmt else ''
+        odr = "ORDER BY "   + self.odr if self.odr else ''
+        qry = f"SELECT {fld} FROM {self.tbl} {wre} {lmt} {odr}"
+        self.qry = qry.strip()
+
+        try : 
+            self.rst = self.cur.execute(qry)
+        except sqlite3.OperationalError :
+            self.err = "SQLITE3 OPERATIONAL ERROR"
+            return False
+
+        rst = self.rst.fetchall() if not many else  self.rst.fetchmany(many)
+        self.num = len(rst)
+        if self.num == 0 : return None 
+        if assoc : 
+            temp_list = []
+            col = [x[0] for x in self.rst.description]
+            for row in rst : temp_list.append(dict(zip(col,row)))
+            # many = 1 일 경우 결과값을 dict로 전달하기 위해 temp_list[0]을 리턴 
+            return temp_list[0] if many == 1 else temp_list  
+
+        else :
+            if fld.find(',') == -1 : # 필드가 하나일 경우 
+                rst = [x[0] for x in rst]
+            return rst[0] if many == 1 else rst
+
+
+    def fetch_assoc(self, many=0) :
+        rst = self.rst.fetchall() if not many else  self.rst.fetchmany(many)
+        temp_list = []
+        col = [x[0] for x in self.rst.description]
+        for row in rst : temp_list.append(dict(zip(col,row)))
+        return temp_list[0] if many == 1 else temp_list      
+
+    def fetch(self, many=0) :
+        if    many == 0 : return self.rst.fetchall()
+        elif  many == 1 : return self.rst.fetchmany(1)[0]
+        else  : return self.rst.fetchmany(many)
+
+# sql query 실행 
+
+    def exe(self,qry:str,many:int=0,assoc:bool=False) -> any :
+        qry = qry.strip()
+        try : 
+            self.rst = self.cur.execute(qry)
+        except sqlite3.OperationalError :
+            return "SQLITE3 OPERATIONAL ERROR"
+
+        if qry.upper().startswith(('SELECT','PRAGMA')) : 
+            return self.fetch(many) if not assoc else self.fetch_assoc(many)
+        else :
+            self.con.commit()
+            return "The job you requested is done"
+       
+    def line(self,qry) :
+        return self.exe(qry,many=1,assoc=True)
+
+
+    def commit_many(self,qry) : 
+        qry2 = qry.split(';')
+        for x in qry2 :
+            try : self.cur.execute(x) 
+            except sqlite3.OperationalError : return "SQLITE3 OPERATIONAL ERROR"
+        
+        self.con.commit()
+        return "The job you requested is done"
+
+
+# 테이블 리스트 및 생성
+
+    def table_list(self) :
+        self.rst = self.cur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name")
+        tmp = self.fetch()
+        return [ t[0] for t in tmp]
+
+    def table_delete_from_no(self,tbl,no) : 
+        sql = f"DELETE FROM {tbl} WHERE no = {no}"
+        self.cur.execute(sql) ; self.con.commit()       
+
+# string convert for DB
+    def qry_select(self,sel,tbl,wre=None) :
+        qry = f"SELECT {sel} FROM {tbl}"
+        if wre : 
+            qry += " WHERE "
+            if type(wre) is str : qry += wre
+            elif type(wre) is dict : qry += ' and '.join(["{}='{}'".format(k,v) for k,v in wre.items()])
+        return qry 
+
+
+    def qry_insert(self,tbl,P:dict) :
+        keys = self.qry_str_key(P)
+        vals = self.qry_str_val(P)
+        qry  = f"INSERT INTO {tbl} ({keys}) VALUES ({vals})"
+        return qry
+
+    def qry_str_key(self,S:dict) : 
+        if type(S) is dict : S = list(S.keys()) 
+        if type(S) is list : return ','.join(S)
+        if type(S) is str  : return S
+        else : return False
+
+    def qry_str_val(self,S:dict) :
+        if type(S) is dict : S = list(S.values()) 
+        if type(S) is list : return f"{S}"[1:-1]
+        if type(S) is str  : return S
+        else : return False
+
+    def qry_update(self,tbl:str,S:dict,con=None) :
+        tmp = ''
+        for key, val in S.items() :
+            if val == 'None':  tmp += f"{key}= null,"
+            else            :  tmp += f"{key}='{val}',"
+        tmp = tmp[:-1] + " "  
+        wre = ' WHERE ' + con if con else ''  
+        qry = f"UPDATE {tbl} SET {tmp} {wre}"
+        return qry 
+
+    def insert_from_post(self,tbl,S=None) :
+        P    = S if S else self.SYS.D['post'] 
+        qry  = self.qry_insert(tbl,P)
+        self.exe(qry)
+
+
---
+++ mytask/stocks_update_task.py
@@ -0,0 +1,73 @@
+from myutils.DB import DB
+import myutils.my_utils as my
+import FinanceDataReader as fdr
+
+class SU :
+    
+    def __init__(self) :
+
+        self.mydb = DB('stocks')
+        self.skey = self.mydb.one("SELECT p_data_01 FROM my_keep_data WHERE no=1")
+
+
+    def stocks_update(self) :
+
+        today = my.timestamp_to_date(opt=7)
+        week_day = my.dayofdate(today)
+
+        if week_day in ['일','월'] :
+            message = f"[{today}] {week_day}요일 : Good morning !"
+            my.post_slack(message)
+            return
+
+        old = my.dayofdate(today,delta=-10)[0]
+        qry = f"SELECT distinct add1 FROM h_stockHistory_board WHERE add1 > '{old}'"
+        codes = self.mydb.exe(qry)
+        codes = [x[0] for x in codes]
+        for cdx in codes : self.update_stock(cdx)
+
+        self.mydb.close()
+
+        message = f"[{today}] 주가 정보 업데이트를 완료하였습니다"
+        my.post_slack(self.skey,message)
+
+    def update_stock(self,cdx) :
+
+        self.mydb.tbl, self.mydb.wre = ('h_stockHistory_board',f"add1='{cdx}'")
+        b_date = self.mydb.get("max(add0)",many=1,assoc=False)
+        e_date = my.timestamp_to_date(opt=7)
+
+        df = fdr.DataReader(cdx,start=b_date, end=e_date)
+        df = df.astype({'Volume':'int'})
+
+        Date    = [x.strftime('%Y-%m-%d') for x in df.index]
+        Change  = [round(x,3) for x in df['Change']]
+
+        cnt = len(Date)
+        Up  = [0]*cnt
+        Dn  = [0]*cnt
+
+        for i in range(1,cnt) :
+            if df['Close'][i] <  df['Close'][i-1] : Dn[i] = Dn[i-1] + 1 
+            if df['Close'][i] >= df['Close'][i-1] : Up[i] = Up[i-1] + 1 
+
+        ohlc = list(zip(Date,df['Open'],df['High'],df['Low'],df['Close'],df['Volume'],Change,Up,Dn))
+
+        self.mydb.exe(f"DELETE FROM {self.mydb.tbl} WHERE add0 >= '{b_date}' AND add1='{cdx}'")
+
+        db_keys = "add0,add4,add5,add6,add3,add7,add8,add9,add10,add1,add2,uid,uname,wdate,mdate"
+        time_now = my.now_timestamp()
+        cdx = cdx.upper()
+        
+        for row in ohlc :
+            row2 = list(row)    
+            row2 += [cdx,cdx,'comphys','정용훈',time_now,time_now]
+            values = str(row2)[1:-1]
+            sql = f"INSERT INTO {self.mydb.tbl} ({db_keys}) VALUES({values})"
+            self.mydb.exe(sql)
+
+
+my_stocks = SU()
+my_stocks.stocks_update()
+
+
