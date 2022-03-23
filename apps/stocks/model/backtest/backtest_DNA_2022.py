b4a9f551671460e3036ef110dcf24ff5ce74f92a
comphys <comphys@gmail.com>
2022-03-24 08:27:13 +0900

    20220324 v2

--- apps/stocks/model/backtest/backtest_DNA_2022.py
+++ apps/stocks/model/backtest/backtest_DNA_2022.py
@@ -365,7 +365,7 @@ class M_backtest_DNA_2022(Model) :
         self.M['연속상승']  = int(self.old_price_trace('UP'))
 
         self.M['progress'] = float(self.D['progress'])
-        if self.M['progress'] > 70 : self.M['progress'] = self.D['progress'] = 50
+        if self.M['progress'] > 50 : self.M['progress'] = self.D['progress'] = 50
  
         총매수금 = int(self.D['init_capital'] * self.M['progress']/100)
         self.M['평균단가']  = self.M['당일종가']
