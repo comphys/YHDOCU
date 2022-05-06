5bbad2986f4ec36308cac38ebc6f407ea281392b
comphys <comphys@gmail.com>
2022-05-06 20:24:58 +0900

    220506 v5

--- apps/stocks/model/page/dashboard2.py
+++ apps/stocks/model/page/dashboard2.py
@@ -51,16 +51,19 @@ class M_dashboard2(Model) :
         self.D['둘째상황'] = self.outcome('h_daily_second_board')
         self.D['셋째상황']  = self.outcome('h_daily_third_board')
 
-        self.D['평가합계'] = float(self.D['첫째상황'][1].replace(',','')) + float(self.D['둘째상황'][1].replace(',','')) + float(self.D['셋째상황'][1].replace(',',''))
-        self.D['투자합계'] = float(self.D['첫째상황'][0].replace(',','')) + float(self.D['둘째상황'][0].replace(',','')) + float(self.D['셋째상황'][0].replace(',',''))
+        fx = float(self.D['첫째상황'][0].replace(',','')); fy = float(self.D['첫째상황'][1].replace(',',''))
+        sx = float(self.D['둘째상황'][0].replace(',','')); sy = float(self.D['둘째상황'][1].replace(',',''))
+        tx = float(self.D['셋째상황'][0].replace(',','')); ty = float(self.D['셋째상황'][1].replace(',',''))
+         
+        self.D['평가합계'] = fy + sy + ty
+        self.D['투자합계'] = fx + sx + tx
         self.D['손익현황'] = self.D['평가합계'] - self.D['투자합계']
         self.D['합수익률'] = self.D['손익현황'] / self.D['투자합계'] * 100
         self.D['total_rate'] = f"{self.D['합수익률']:.1f}"
         
-        # self.D['평가합계'] = f"{self.D['평가합계']:,.0f}"
-        # self.D['투자합계'] = f"{self.D['투자합계']:,.0f}"
-        # self.D['손익현황'] = f"<span style='color:#ced8f6'>{self.D['손익현황']:,.2f}</span>"  if self.D['손익현황'] < 0 else f"<span style='color:#f6cece'>{self.D['손익현황']:,.2f}</span>"
-        # self.D['합수익률'] = f"<span style='color:#ced8f6'>{self.D['합수익률']:,.1f}%</span>" if self.D['합수익률'] < 0 else f"<span style='color:#f6cece'>{self.D['합수익률']:,.1f}%</span>"
+        self.D['fy_w'] = int( 1470 * fy / self.D['평가합계'])
+        self.D['sy_w'] = int( 1470 * sy / self.D['평가합계'])
+        self.D['ty_w'] = int( 1470 * ty / self.D['평가합계'])
 
         ckday = self.DB.one("SELECT max(add0) FROM h_stockHistory_board")
 
@@ -70,13 +73,11 @@ class M_dashboard2(Model) :
         # self.D['확인요일']  = ut.dayofdate(ckday)
 
         # # 전략
-        # tbl = ('h_daily_first_board','h_daily_second_board','h_daily_third_board')
-        # tle = ('첫째계좌','둘째계좌','셋째계좌')
-        # ttt = ('첫째전략','둘째전략','셋째전략')
+        tbl = ('h_daily_first_board','h_daily_second_board','h_daily_third_board')
 
-        # for i, tbl in enumerate(tbl) :
-        #     self.DB.tbl, self.DB.wre = (tbl,f"add0='{self.D['확인날자']}' and add19='시즌진행'")
-        #     D = self.DB.get_line("*")
+        for i, tbl in enumerate(tbl) :
+            self.DB.tbl, self.DB.wre = (tbl,f"add0='{self.D['확인날자']}' and add19='시즌진행'")
+            D = self.DB.get_line("*")
         #     days = self.DB.one(f"SELECT count(no) FROM {tbl} WHERE add19='시즌진행'")
         #     if D : self.D[ttt[i]] = self.print_out(D,title=tle[i],days=days)
         #     else : self.D[ttt[i]] = f"<div style='text-align:center'>{self.D['확인날자']}({self.D['확인요일']}) 일에 대한 {tle[i]} 정보가 없습니다</div>"            
@@ -101,9 +102,19 @@ class M_dashboard2(Model) :
         기초자금 = f"{기초자금:,.0f}"
         현재자금 = f"{현재자금:,.0f}"
         가용잔액 = f"{가용잔액:,.2f}"
-        profit_rate = f"{최종수익률:.1f}" 
-        최종수익 = f"<span style='color:#ced8f6'>{최종수익:,.2f}</span>" if 최종수익 < 0 else f"<span style='color:#f6cece'>{최종수익:,.2f}</span>"
-        최종수익률 = f"<span style='color:#ced8f6'>{최종수익률:,.1f}%</span>" if 최종수익률 < 0 else f"<span style='color:#f6cece'>{최종수익률:,.1f}%</span>"
+
+        최종수익 = f"{최종수익:,.2f}" 
+        최종수익률 = f"{최종수익률:,.1f}"
 
         out = [기초자금,현재자금,최종수익,최종수익률,'S'+LD['add2'],LD['add4'],LD['add10'],LD['add9'],f"{float(LD['add15']):,.1f}",f"{float(LD['add11']):,.0f}",가용잔액]
         return out
+
+    def strategy(self,D,days=0) :
+        output = ['&nbsp','&nbsp','&nbsp','&nbsp','&nbsp','&nbsp']
+        if int(D['buy11']) :  output[0] = '일반매수' ; output[1] = D['buy11']; output[2] = D['buy12']
+        if int(D['buy31']) :  output[0] = '추종매수' ; output[1] = D['buy31']; output[2] = D['buy32']
+        if int(D['buy41']) :  output[0] = '추가매수' ; output[1] = D['buy41']; output[2] = D['buy42']
+        if int(D['buy51']) :  output[0] = '전략매수' ; output[1] = D['buy51']; output[2] = D['buy52']
+        if int(D['buy21']) :  output[0] = '터닝매수' ; output[1] = D['buy21']; output[2] = D['buy22']
+
+        return output
