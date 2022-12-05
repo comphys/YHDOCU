a6a3430e1e92d734f5926683da77982d31084010
comphys <comphys@gmail.com>
2022-12-05 16:23:23 +0900

    221205 v5

--- apps/stocks/control/boards/stock_victory.py
+++ apps/stocks/control/boards/stock_victory.py
@@ -32,10 +32,11 @@ class Stock_victory(Control) :
         self.DB.tbl, self.DB.wre = ('h_stock_strategy_board',f"add0='{self.M['매매전략']}'")
         self.S = self.DB.get_line('add2,add9,add10,add17,add18,add25')
         self.M['분할횟수']  = int(self.S['add2'])
+        self.M['큰단가치']  = 1 + float(self.S['add5'])/100   # 매수첫날 구매가 범위
         self.M['첫매가치']  = 1 + float(self.S['add9'])/100
         self.M['둘매가치']  = 1 + float(self.S['add10'])/100
         self.M['강매시작']  = int(self.S['add17'])
-        self.M['강매가치']  = 1 + float(self.S['add18']) / 100
+        self.M['강매가치']  = 1 + float(self.S['add18'])/100
         self.M['위매비중']  = int(self.S['add25'])
 
         # 매수 매도 초기화
@@ -120,7 +121,7 @@ class Stock_victory(Control) :
 
         if  self.M['경과일수'] == 0 :
             self.M['전매수량'] = math.ceil(self.M['일매수금']/self.M['당일종가'])
-            self.M['전매수가'] = self.M['당일종가']
+            self.M['전매수가'] = self.M['당일종가'] * self.M['큰단가치']
             return
 
         if self.M['전매도가'] : 매수단가 = min(self.M['당일종가'],self.M['전매도가'])
