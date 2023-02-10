7bd6f00009db15c538250063526836be8acf2ba4
comphys <comphys@gmail.com>
2023-02-11 01:21:01 +0900

    230211 v0

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 class Config(object):
     DEBUG = True
-    ENV = 'development'
     SECRET_KEY = 'JungYhKimJhJungYj'
 
 
 class ProductionConfig(object):
-    ENV = 'Production'
     DEBUG = False
     SECRET_KEY = 'JungYhKimJhJungYj'
--- start_2.bat
+++ start_2.bat
@@ -1,3 +1,2 @@
 set FLASK_APP=index.py
-set FLASK_ENV=production
-python -m flask run --port=5000 --host=127.0.0.1 --lazy-loading 
+python -m flask --no-debug run 
