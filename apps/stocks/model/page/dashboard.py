18eef2c3f6efa076eb506c0ebaf5bf86fb3194a9
comphys <comphys@gmail.com>
2023-02-15 22:45:05 +0900

    230215 v4

--- apps/stocks/model/page/dashboard.py
+++ apps/stocks/model/page/dashboard.py
@@ -12,7 +12,7 @@ class M_dashboard(Model) :
         ckdate = self.DB.get_one("max(add0)")
         self.DB.wre = f"add0='{ckdate}'"
 
-        out = self.DB.get_line("sub2,sub19,sub3,sub20")
+        out = self.DB.get_line("sub2,sub19,sub3,sub20,sub25,sub26,add17,sub28")
         매수수량 = int(out['sub2'])
         매수가격 = float(out['sub19']) 
         매수가액 = 매수수량 * 매수가격
@@ -28,6 +28,12 @@ class M_dashboard(Model) :
         self.D['매도수량'] = f"{매도수량:>3d}"
         self.D['매도가격'] = f"${매도가격:,.2f}"
         self.D['매도가액'] = f"${매도가액:,.2f}"
+
+        self.D['총입금'] = f"{float(out['sub25']):,.0f}"
+        self.D['총출금'] = f"{float(out['sub26']):,.0f}"
+        self.D['현재액'] = f"{float(out['add17']):,.0f}"
+        self.D['수익금'] = f"{float(out['add17'])-float(out['sub25'])-float(out['sub26']):,.0f}"
+        self.D['수익률'] = f"{float(out['sub28']):,.1f}"
 
 
  
