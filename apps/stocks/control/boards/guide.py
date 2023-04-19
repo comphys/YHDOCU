44c5ac6ea993b699ee989d1a0836c3905e32f638
comphys <comphys@gmail.com>
2023-04-20 00:49:13 +0900

    230420 v1

---
+++ apps/stocks/control/boards/guide.py
@@ -0,0 +1,25 @@
+from system.core.load import Control
+
+class Guide(Control) : 
+
+    def _auto(self) :
+        self.DB = self.db('stocks')
+        self.bid   = self.parm[0]
+        self.board = 'h_'+self.bid+'_board'
+    
+    def empty(self) :
+        qry = f"DELETE FROM {self.board}"
+        self.DB.exe(qry)
+        return self.moveto('board/list/'+self.bid)
+    
+    def insertPick(self) :
+        pickDate = self.parm[1]
+        self.DB.tbl, self.DB.wre = ('h_VICTORY_board',f"add0='{pickDate}'")
+
+        line = self.DB.get_line("*")
+        del line['no']
+        
+        qry=self.DB.qry_insert(self.board,line)
+        self.DB.exe(qry)
+        
+        return self.moveto('board/list/'+self.bid)
