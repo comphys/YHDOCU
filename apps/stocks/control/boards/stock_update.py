f05fb60216beb43e3bb8a16f2845092395445e08
comphys <comphys@gmail.com>
2023-07-08 09:02:00 +0900

    230708 v1

--- apps/stocks/control/boards/stock_update.py
+++ apps/stocks/control/boards/stock_update.py
@@ -15,15 +15,15 @@ class Stock_update(Control) :
         USER = self.DB.get("*",many=1, assoc=True)
 
         opt = self.parm[0]
-        codes = ['SOXL','JEPQ']
+        # codes = ['SOXL','JEPQ']
+        codes = 'SOXL'
         
-        for cdx in codes :
-            if opt == 'stockdio' :
-                self.update_stockdio(cdx,USER)
-                time.sleep(5)
-            else :
-                self.update_fdr(cdx,USER)
-                time.sleep(2)
+        if opt == 'stockdio' :
+            self.update_stockdio(cdx,USER)
+            time.sleep(5)
+        else :
+            self.update_fdr(cdx,USER)
+            time.sleep(2)
             
         self.set_message("종목 업데이트를 완료하였습니다")
         return self.moveto('board/list/stockHistory/csh=on')
