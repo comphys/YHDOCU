9fc4918430e31029ba398c44269d94523a64443d
comphys <comphys@gmail.com>
2023-08-16 20:08:27 +0900

    jbk230816 2008

--- apps/jbk/control/board.py
+++ apps/jbk/control/board.py
@@ -10,19 +10,18 @@ class Board(Control) :
     def _auto(self) :
 
         self.DB = self.db('jbk')
-        
-        self.D['bid']     = self.parm[0] if self.parm else 'estimate'
-        self.D['tbl']     = 'h_'+self.D['bid']+'_board'
-        self.DB.tbl, self.DB.wre = ("h_board_config",f"bid='{self.D['bid']}'")
-        self.D['BCONFIG'] = self.DB.get("*",many=1,assoc=True)
+        if 'JBK_KHJM' in session :
+            self.D['bid']     = self.parm[0] if self.parm else 'estimate'
+            self.D['tbl']     = 'h_'+self.D['bid']+'_board'
+            self.DB.tbl, self.DB.wre = ("h_board_config",f"bid='{self.D['bid']}'")
+            self.D['BCONFIG'] = self.DB.get("*",many=1,assoc=True)
 
-        self.skin = 'board/'+self.D['BCONFIG']['skin']
-        self.model('board-board_main')
-        self.D['DOCU_ROOT'] = self.C['DOCU_ROOT']
-        if not session['CSH'] : session['CSH'] = {}
-
-
+            self.skin = 'board/'+self.D['BCONFIG']['skin']
+            self.model('board-board_main')
+            self.D['DOCU_ROOT'] = self.C['DOCU_ROOT']
+  
     def list(self) :
+        if not 'JBK_KHJM' in session : return self.moveto('board/login')
         M = self.model('board-board_list')
         M.list_head()
         M.list_main()
@@ -31,6 +30,7 @@ class Board(Control) :
         return self.echo(D)
 
     def body(self) :
+        if not 'JBK_KHJM' in session : return self.moveto('board/login')
         M = self.model('board-board_list')
         M.list_head()
         M.list_main()
@@ -41,6 +41,7 @@ class Board(Control) :
         return self.echo(D)      
 
     def write(self) :
+        if not 'JBK_KHJM' in session : return self.moveto('board/login')
         self.D['Mode'] = 'write'
         M = self.model('board-board_write')
         M.write_main()
@@ -48,27 +49,18 @@ class Board(Control) :
         self.get_message()
         return self.echo(D)
 
-    def add_body(self) :
-        self.D['Mode'] = 'add_body'
-        self.D['No'] = self.gets['no']
-        self.D['Brother']  = int(self.gets.get('brother','0')) 
-        self.D['Form_act'] = self.D['_bse'] + 'boards-action/save/' + self.D['bid']
-        o_no = self.D['Brother'] if self.D['Brother'] > 0 else self.D['No']
-        sql = f"SELECT add0 FROM h_{self.parm[0]}_board WHERE no={o_no}"
-        self.D['B_title'] = self.DB.one(sql)
-        self.D['Form_act'] += "/no=" + self.D['No']
-        self.D['MustCheck'] = "'add0','추가 타이틀'"
-        if self.D['BCONFIG']['width'] : w_width  = int( (self.D['BCONFIG']['width']).replace('px','') )
-        else : w_width = 815
-        self.D['w_width1'] = str(w_width + 80)+'px'
-        self.D['w_width2'] = str(w_width) + 'px'
-        D={'skin': self.skin + '/write/' + self.D['BCONFIG']['sub_write']}
-        return self.echo(D)
 
-    def modify(self,mode='modify') :
+    def modify(self) :
+        if not 'JBK_KHJM' in session : return self.moveto('board/login')
         self.D['Mode'] = 'modify'
         M = self.model('board-board_write')
         M.write_main()
         D={'skin': self.skin + '/write/' + self.D['BCONFIG']['sub_write']}
         return self.echo(D)
+    
+    def login(self) :
+        session['JBK_KHJM'] = 'ESTIMATE_PROGRAM 20230816'
+        session['CSH'] = {}
+        return self.moveto('board/list')
+       
 
