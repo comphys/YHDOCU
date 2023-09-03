e9e738f56c83866d1d84eb2ef56c9794237d92a8
comphys <comphys@gmail.com>
2023-09-03 18:39:09 +0900

    230903 1837

--- apps/stocks/control/pages/pajax.py
+++ apps/stocks/control/pages/pajax.py
@@ -7,10 +7,19 @@ class Pajax(Control) :
         
     def eng_reply(self) :
         text = self.D['post']['txt']
-        text = f'"{text}" 를 영어로 번역해 주세요.'
+        opt  = self.D['post']['opt']
+        system_role = ''
+
+        if  opt == 'eng_teacher' : 
+            system_role = self.DB.store('openai_english_teacher')
+            text = f'"{text}" 를 영어로 번역해 주세요.'
+        
+        elif opt == 'gen_teacher' :
+            system_role = self.DB.store('openai_kind_teacher')
+            
+        messages=[{'role':'system','content':system_role}]
         openai.api_key = self.DB.store('openai_key')
-        system_role = self.DB.store('openai_english_teacher')
-        messages=[{"role": "system", "content": system_role}]
+        
         messages.append({"role":"user","content":f"{text}"})
         
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",temperature=0.8,messages=messages)
