ed3e814487da0a272dc1df7a13c04e534ebfe74d
comphys <comphys@gmail.com>
2023-09-03 15:51:31 +0900

    230903 1551

--- apps/stocks/control/pages/pajax.py
+++ apps/stocks/control/pages/pajax.py
@@ -8,11 +8,10 @@ class Pajax(Control) :
     def eng_reply(self) :
         text = self.D['post']['txt']
         openai.api_key = self.DB.store('openai_key')
-        messages=[{"role": "system", "content": "You are an English-Korean translation machine. Your role is to trannslate my quesions into English. And give me more than 3 options if you have. prefix your answers with a number."},{}]
-
-        messages[1] = {"role":"user","content":f"{text}"}
+        system_role = self.DB.store('openai_english_teacher')
+        messages=[{"role": "system", "content": system_role}]
+        messages.append({"role":"user","content":f"{text}"})
+        
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",temperature=0.8,messages=messages)
-
         assistant_content = completion.choices[0].message['content'].strip()
-
         return assistant_content
