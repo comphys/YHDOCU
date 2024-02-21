39d4154a96d8262e182c7d597bcc84d4006fc638
comphys <comphys@gmail.com>
2024-02-21 17:59:57 +0900

    240221 1759

--- apps/stocks/model/page/dashboard2.py
+++ apps/stocks/model/page/dashboard2.py
@@ -135,22 +135,41 @@ class M_dashboard2(Model) :
 
         self.D['환율표기']  = f"{self.D['현재환율']:,.1f}"
         for odr in [0,1,2] :
-            cond = f"WHERE add0 BETWEEN '{self.M['구간시작']}' AND '{self.M['구간종료']}'" if self.M['구간종료'] else ''
+            cond = f"WHERE add0 <= '{self.M['구간종료']}'" if self.M['구간종료'] else '' 
             qry = f"SELECT add10, add17, sub25, sub26 FROM {self.M['boards'][odr]} {cond} ORDER BY add0 DESC LIMIT 1"
             qrs = self.DB.exe(qry)
             if not qrs : break
             rst = qrs[0] 
             key = str(odr+1)
-            self.D['자산분배'+key] = rst[0]
-            self.D['자산총액'+key] = float(rst[1])
-            self.D['총입금액'+key] = float(rst[2])
-            self.D['총출금액'+key] = float(rst[3])
-            self.D['증가비율'+key] = round(self.D['자산총액'+key]/(self.D['총입금액'+key]-self.D['총출금액'+key])* 100,2)
+            self.D['E자산분배'+key] = rst[0]
+            self.D['E자산총액'+key] = float(rst[1])
+            self.D['E총입금액'+key] = float(rst[2])
+            self.D['E총출금액'+key] = float(rst[3])
+            self.D['E순자산액'+key] = self.D['E총입금액'+key]-self.D['E총출금액'+key]
+            self.D['E증가비율'+key] = round(self.D['E자산총액'+key]/self.D['E순자산액'+key]* 100,2)
             
-        총가치합 = self.D['자산총액1']+self.D['자산총액2']+self.D['자산총액3']
-        총입출입 = self.D['총입금액1']-self.D['총출금액1']+self.D['총입금액2']-self.D['총출금액2']+self.D['총입금액3']-self.D['총출금액3']
+        총가치합 = self.D['E자산총액1']+self.D['E자산총액2']+self.D['E자산총액3']
+        총입출입 = self.D['E총입금액1']-self.D['E총출금액1']+self.D['E총입금액2']-self.D['E총출금액2']+self.D['E총입금액3']-self.D['E총출금액3']
         
-        self.D['증가비율0'] = round(총가치합/총입출입 * 100,2)    
+        self.D['E증가비율0'] = round(총가치합/총입출입 * 100,2)    
+
+        # for odr in [0,1,2] :
+        #     cond = f"WHERE add0 <= '{self.M['구간시작']}'" if self.M['구간시작'] else '' 
+        #     qry = f"SELECT add17, sub25, sub26 FROM {self.M['boards'][odr]} {cond} ORDER BY add0 DESC LIMIT 1"
+        #     qrs = self.DB.exe(qry)
+        #     if not qrs : break
+        #     rst = qrs[0] 
+        #     key = str(odr+1)
+        #     self.D['S자산총액'+key] = float(rst[0])
+        #     self.D['S총입금액'+key] = float(rst[1])
+        #     self.D['S총출금액'+key] = float(rst[2])
+        #     self.D['S순자산액'+key] = self.D['S총입금액'+key]-self.D['S총출금액'+key]
+        #     self.D['S증가비율'+key] = round(self.D['S자산총액'+key]/(self.D['S총입금액'+key]-self.D['S총출금액'+key])* 100,2)
+
+        # 구간입금 = 나중총입금 - 처음총입금
+        # 구간출금 = 나중총출금 - 처음총출금     
+        # 구간 자산증가 = 총액2 - 총액1 - 구간입금 + 구간출금
+
             
     def show_strategy(self,ST) :
 
