a4d6026c9d09028ce90555048953908f5ddb744f
comphys <comphys@gmail.com>
2024-04-16 07:37:13 +0900

    240416 0737

--- apps/stocks/lib/stock.py
+++ apps/stocks/lib/stock.py
@@ -9,10 +9,10 @@ class STOCK :
         self.headers = {'User-Agent' : ('Mozilla/5.0 (Windows NT 10.0;Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36')} 
 
     def get_history(self,code,minDate) :
-        url = f"https://finance.yahoo.com/quote/{code}/history?p={code}"
+        url = f"https://finance.yahoo.com/quote/{code}/history"
         temp = requests.get(url, headers=self.headers)
         soup = bs(temp.text,'lxml')
-        html = soup.select("#Col1-1-HistoricalDataTable-Proxy>section>div>table>tbody>tr")[0:20]
+        html = soup.select("#nimbus-app > section > section > section > article > div.container > div.table-container.svelte-ta1t6m > table > tbody > tr")[0:20]
 
         today = my.timestamp_to_date(ts='now',opt=7)
         SH = []
