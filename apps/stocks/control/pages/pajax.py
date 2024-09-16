2b0937bc2f28b658719ae7daf40f6487db1fd6a1
comphys <comphys@gmail.com>
2024-09-16 13:05:13 +0900

    240916 1305

--- apps/stocks/control/pages/pajax.py
+++ apps/stocks/control/pages/pajax.py
@@ -110,11 +110,12 @@ class Pajax(Control) :
 
         V_board = 'h_IGUIDE_board'
 
-        V_date = self.DB.one(f"SELECT add0 FROM {V_board} WHERE add0 <= '{s_date}' and sub12 = '1' ORDER BY {order} LIMIT 1")
+        V_date  = self.DB.one(f"SELECT add0 FROM {V_board} WHERE add0 <= '{s_date}' and sub12 = '1' ORDER BY {order} LIMIT 1")
         V_money = self.DB.one(f"SELECT add3 FROM {V_board} WHERE add0 < '{V_date}' and sub12= '0' ORDER BY {order} LIMIT 1")
+        V_mode  = self.DB.one(f"SELECT sub7 FROM {V_board} WHERE add0 = '{V_date}'")
         
         if  not (V_date and V_money) : 
             V = {'date':'None','msg':f"주어진 날자 이전의 동기화된 데이타는 존재하지 않습니다."}
         else :
-            V = {'date':V_date,'V_money':f"{float(V_money):,.2f}"}
+            V = {'date':V_date,'V_money':f"{float(V_money):,.2f}",'V_mode':V_mode}
         return self.json(V)
