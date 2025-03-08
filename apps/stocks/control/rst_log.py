815c5b537b071b1139fc2ff2579d56ad8a129ae9
comphys <comphys@gmail.com>
2025-03-08 20:34:40 +0900

    250308 1834

--- apps/stocks/control/rst_log.py
+++ apps/stocks/control/rst_log.py
@@ -1,15 +1,16 @@
 from system.core.load import Control
 from flask import session
 
-class Rst_control(Control) :
+class Rst_log(Control) :
 
     def index(self) :
         if not 'N_NO' in session : return self.moveto('board/login')
         self.DB = self.db('stocks')
         
         RST = self.load_app_lib('rst')
+        RST.op = 'jrst'
         theDate = self.parm[1]
-        tactic  = self.parm[0]
+        tactic  = self.parm[0].upper()
 
         RST.do_tacticsLog(theDate)
         D = RST.get_tacticLog(theDate,tactic)
