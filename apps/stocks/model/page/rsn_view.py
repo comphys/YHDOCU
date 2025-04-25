926193aae52dbcdcc9176925b0eddf9024cec2ea
comphys <comphys@gmail.com>
2025-04-25 15:32:53 +0900

    250425 1532

--- apps/stocks/model/page/rsn_view.py
+++ apps/stocks/model/page/rsn_view.py
@@ -13,7 +13,7 @@ class M_rsn_view(Model) :
         # 기본 값
         self.D['종목코드'] = 'SOXL'
         투자자금 = self.DB.parameters('TC010')
-        자금배분 = my.sf(self.DB.parameters('TC012'))
+        자금배분 = my.sf(self.DB.parameters('TC011'))
         
         self.D['기회자금'] = round(투자자금*자금배분[0]/100,2)
         self.D['안정자금'] = round(투자자금*자금배분[1]/100,2)
