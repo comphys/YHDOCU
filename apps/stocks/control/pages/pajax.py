5313c2e20f35a2f95fed12e76b1e848be17f5e4d
comphys <comphys@gmail.com>
2025-06-12 07:03:49 +0900

    250612 0703

--- apps/stocks/control/pages/pajax.py
+++ apps/stocks/control/pages/pajax.py
@@ -1,37 +1,10 @@
 from system.core.load import Control
 import system.core.my_utils as my
-# import openai
 
 class Pajax(Control) :
     def _auto(self) :
         self.DB = self.db('stocks')
         
-    # def eng_reply(self) :
-    #     text = self.D['post']['txt']
-    #     opt  = self.D['post']['opt']
-    #     system_role = ''
-
-    #     if  opt == 'eng_teacher' : 
-    #         system_role = self.DB.store('openai_english_teacher')
-    #         text = f'"{text}" 를 영어로 번역해 주세요.'
-
-    #     if  opt == 'kor_teacher' : 
-    #         system_role = self.DB.store('openai_kind_teacher')
-    #         text = f'"{text}" 를 한글로 번역해 주세요.'
-                    
-    #     elif opt == 'gen_teacher' :
-    #         system_role = self.DB.store('openai_kind_teacher')
-            
-    #     messages=[{'role':'system','content':system_role}]
-        
-    #     openai.api_key = self.DB.store('openai_key')
-        
-    #     messages.append({"role":"user","content":f"{text}"})
-        
-    #     completion = openai.chat.completions.create(model="gpt-3.5-turbo",temperature=0.7,messages=messages,max_tokens=2048)
-    #     assistant_content = completion.choices[0].message.content.strip()
-    #     return assistant_content
-    
     def update_parameters(self) :
 
         key = self.D['post']['key']
