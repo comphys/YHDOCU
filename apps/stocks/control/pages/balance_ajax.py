5b619bc1e6b38e255275e6f79c07eda727551fa3
comphys <comphys@gmail.com>
2025-10-10 07:41:27 +0900

    251010 0741

---
+++ apps/stocks/control/pages/balance_ajax.py
@@ -0,0 +1,117 @@
+from system.core.load import Control
+import system.core.my_utils as my
+
+class Balance_ajax(Control) :
+
+    def _auto(self) :
+        
+        self.DB = self.db('stocks')
+        self.msg  = ''
+    
+    
+    def re_balance(self) :
+        
+        s_ch = self.D['post']['choice']
+        m_in = self.D['post']['money_in']
+        m_ex = self.D['post']['money_ex']
+        m_nw = self.D['post']['money_nw']
+        d_ch = self.D['post']['start_date']
+        
+        PD = {'s_ch':s_ch, 'm_in':m_in, 'm_ex':m_ex, 'm_nw':m_nw, 'd_ch':d_ch}
+     
+        if   s_ch == 'RSN'  : self.rsn(m_in,m_ex,m_nw,d_ch)
+        elif s_ch == 'N315' : self.n315(m_in,m_ex,m_nw,d_ch)
+        else : self.lucky(m_in,m_ex,m_nw,d_ch)
+
+        return self.msg
+   
+    def rsn(self,m_in,m_ex,m_nw,d_ch) :
+
+        LD = self.DB.last_record('h_rsnLog_board')
+
+        if  LD['add17'] != '수익실현' : 
+            self.msg = '현재 시즌 진행 중입니다. 시즌 종료 후 작업하시기 바랍니다.'
+            return
+        
+        if  LD['add0'] >= d_ch :
+            self.msg = '새로운 시작 날자는 최종 진행 날자 이후여야 합니다. 날자를 재 지정 하시기 바랍니다.'
+            return            
+
+        or_mon = my.sv(LD['add18'])
+        os_mon = my.sv(LD['add19'])
+        on_mon = my.sv(LD['add20'])
+        ov_mon = my.sv(LD['v_03'])
+
+        # 잔액 및 가치합계 재 설정
+        total = or_mon + os_mon + ov_mon  # R잔액 + S잔액 + N잔액
+        
+        if my.sv(m_in) : total += my.sv(m_in)
+        if my.sv(m_ex) : total -= my.sv(m_ex)
+        if my.sv(m_nw) : total  = my.sv(m_nw)
+
+        ST = self.DB.parameters_dict('매매전략/RSN')
+        allot = my.sf(ST['TC011'])
+        
+        r_mon  = round(total*allot[0]/100,2);  LD['r_14'] = LD['r_03'] = LD['add18'] = f"{r_mon:.2f}"
+        s_mon  = round(total*allot[1]/100,2);  LD['s_14'] = LD['s_03'] = LD['add19'] = f"{s_mon:.2f}"
+        n_mon  = total - r_mon - s_mon;        LD['n_14'] = LD['n_03'] = LD['add20'] = f"{n_mon:.2f}"
+        v_mon  = total;                        LD['v_14'] = LD['v_03'] = LD['add12'] = LD['add14'] = f"{v_mon:.2f}"
+
+        if r_mon > or_mon : LD['r_01'] = f"{r_mon - or_mon:.2f}"
+        else : LD['r_02'] = f"{or_mon - r_mon:.2f}"
+        if s_mon > os_mon : LD['s_01'] = f"{s_mon - os_mon:.2f}"
+        else : LD['s_02'] = f"{os_mon - s_mon:.2f}"
+        if n_mon > on_mon : LD['n_01'] = f"{n_mon - on_mon:.2f}"
+        else : LD['n_02'] = f"{on_mon - n_mon:.2f}"
+        if v_mon > ov_mon : LD['v_01'] = f"{v_mon - ov_mon:.2f}"
+        else : LD['v_02'] = f"{ov_mon - v_mon:.2f}"
+
+        # 기초수량 재 선정
+        r_bm = int(r_mon/ST['TV010']); LD['r_15'] = str(r_bm) # 일매수금
+        r_bq = my.ceil(r_bm / my.sv(LD['add3'])); LD['r_16'] = str(r_bq) # 기초수량
+        LD['r_17'] = LD['r_16'] # 매수수량
+
+        s_bm = int(s_mon/ST['TV010']); LD['s_15'] = str(s_bm) # 일매수금
+        s_bq = my.ceil(s_bm / my.sv(LD['add3'])); LD['s_16'] = str(s_bq) # 기초수량
+
+        n_allot = my.sf(ST['TN010'])
+        LD['n_16'] = int(n_mon * n_allot[0])
+        LD['n_16'] = str(LD['n_16'])
+
+        v_bm = int(v_mon/ST['TV010']); LD['v_15'] = str(v_bm) # 일매수금
+        v_bq = my.ceil(v_bm / my.sv(LD['add3'])); LD['v_16'] = str(v_bq) # 기초수량
+        LD['v_17'] = LD['v_16'] # 매수수량
+
+
+        LD['content'] = f"투자금액 변경 (기존) {ov_mon:,.2f} > (변경) {v_mon:,.2f}, (변경시작일) {d_ch}" 
+        # 새로운 데이타 
+        del(LD['no']); del(LD['brother']); del(LD['tle_color']); del(LD['reply']); del(LD['hit'])
+        del(LD['v_13']); del(LD['v_24']); del(LD['v_25'])
+        del(LD['r_13']); del(LD['r_24']); del(LD['r_25'])
+        del(LD['s_13']); del(LD['s_24']); del(LD['s_25'])
+        del(LD['n_13']); del(LD['n_24']); del(LD['n_25'])
+
+
+        LD['wdate'] = LD['mdate'] = my.now_timestamp()
+        qry=self.DB.qry_insert('h_rsnLog_board',LD)
+        self.DB.exe(qry)
+
+        # 파라미터 업데이트
+        self.DB.parameter_update('TX050',d_ch)
+        self.DB.parameter_update('TX051',LD['add12'])
+        
+        self.msg = "RSN 에 대한 투자금액 변경작업이 정상적으로 변경되었습니다."
+
+    
+    def n315(self,m_in,m_ex,m_nw,d_ch) :
+
+        LD = self.DB.last_record('h_log315_board')
+        self.info(LD)
+    
+    def lucky(self,m_in,m_ex,m_nw,d_ch) :
+
+        LD = self.DB.last_record('h_log_lucky_board')
+        self.info(LD)
+        
+        
+        
