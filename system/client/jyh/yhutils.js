91aa88ec9e3e4939ed60e91440f3148f8c9014c0
comphys <comphys@gmail.com>
2025-11-21 08:02:41 +0900

    251121 0802

--- system/client/jyh/yhutils.js
+++ system/client/jyh/yhutils.js
@@ -34,8 +34,9 @@ function uri(parameter){
 		case 'gets':return segment
 		case 'base' : return location.protocol+'//'+location.hostname; 
 		case 'linkurl' : return '/'+ segment[0] + '/';
+		case 'linkajax' : return '/'+ segment[0] + '/'+segment[1]+'/ajax/';
 		case 'last' : return segment[cnt-1];
-		case 'thispage' : return '/'+ segment[0] + '/' + segment[1] + '/' + segment[2] + '/';
+		case 'thispage' : return '/'+ segment[0]+'/'+segment[1]+'/'+segment[2]+'/';
 		default: 
 			if (parameter in qry) return qry[parameter];
 			else return '';
