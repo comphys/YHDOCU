c92a53a24a2099db68741b3805f2267edca98b21
comphys <comphys@gmail.com>
2026-04-02 12:08:54 +0900

    260402 1208

---
+++ apps/stocks/model/page/whoin.py
@@ -0,0 +1,18 @@
+from system.core.load import Model
+import system.core.my_utils as my
+
+class M_whoin(Model) :
+
+    def view(self) :
+        
+        # 기본 값
+        with open('whoin.txt','r',encoding='utf-8') as f:
+            content = f.read()
+
+        self.D['로긴정보'] = content.replace('\n','<br>')
+
+
+
+    def action(self) :
+
+        return        
