f607f192534cfe071f7ee223707aab8423097ff3
comphys <comphys@gmail.com>
2026-08-04 19:40:43 +0900

    260804 1940

--- system/core/my_utils.py
+++ system/core/my_utils.py
@@ -101,7 +101,7 @@ def kor_loc_date(opt='Asia/Seoul') :
 def last_stock_day(mydb) :
 
     now = now_to_kordate()
-    ldy = now[0:10] if now[14:19] >= '09:10' else date_yesterday(now[0:10]) 
+    ldy = now[0:10] if now[14:19] > '09:00' else date_yesterday(now[0:10]) 
     delta = -1
     while delta :
         temp = dayofdate(ldy,delta)
