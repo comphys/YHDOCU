561c10ab248b3a782c8ecec563fdfd10564d8466
comphys <comphys@gmail.com>
2026-08-05 18:34:32 +0900

    260805 1834

--- apps/stocks/control/board.py
+++ apps/stocks/control/board.py
@@ -33,7 +33,7 @@ class Board(Control) :
 
             if  self.D['lst_sday'] != self.D['lst_ohlc'] : 
                 OHLC = self.load_app_lib('ohlc')
-                if OHLC.stocks_update('soxl',self.D['lst_sday']) : self.D['lst_ohlc'] = self.D['lst_ohlc']
+                if OHLC.stocks_update('soxl',self.D['lst_sday']) : self.D['lst_ohlc'] = self.DB.last_date('h_stockHistory_board')
                 RSN  =  self.load_bajax('rsnLog','update_log'); RSN()
                 DIY  =  self.load_bajax('logDIY','update_log'); DIY()
                 N315 =  self.load_bajax('log315_ljk','update_log'); N315()
