8ae5ed2757476a691dc2221af3bb01b54c5b0e3e
comphys <comphys@gmail.com>
2026-08-24 21:56:12 +0900

    260824 0956

--- apps/api/control/check.py
+++ apps/api/control/check.py
@@ -1,24 +1,29 @@
 from system.core.load import Control
-import jwt #Pyjwt
+import system.core.my_utils as my
+# import jwt #Pyjwt
 
 class Check(Control) : 
 
 
-    def _auto(self) :
-        self.DB = self.db('stocks')
+    # def _auto(self) :
+    #     self.DB = self.db('stocks')
 
-    def is_valid_token(self) :
-        s_key = self.DB.store('secret_key')
-        try :
-            jwt.decode(self.D['auth'],s_key,algorithms=['HS256'])
-            return True
-        except jwt.InvalidTokenError :
-            return False
+    # def is_valid_token(self) :
+    #     s_key = self.DB.store('secret_key')
+    #     try :
+    #         jwt.decode(self.D['auth'],s_key,algorithms=['HS256'])
+    #         return True
+    #     except jwt.InvalidTokenError :
+    #         return False
 
-    def rsn_check(self):
+    # def rsn_check(self):
 
-        if  self.is_valid_token() :
-            self.DB.parameter_update('TX070',self.D['post']['date'])
-            return self.D['post']['date']
-        else :
-            return "유효하지 않거나 위변조된 토큰입니다"
+    #     if  self.is_valid_token() :
+    #         self.DB.parameter_update('TX070',self.D['post']['date'])
+    #         return self.D['post']['date']
+    #     else :
+    #         return "유효하지 않거나 위변조된 토큰입니다"
+
+    def ip_check(self) :
+
+        return my.get_publicIP()
