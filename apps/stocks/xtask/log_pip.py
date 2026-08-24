de4f226a46425faf3f7e867342cc0a753a9b785d
comphys <comphys@gmail.com>
2026-08-24 17:39:47 +0900

    260824 1739

---
+++ apps/stocks/xtask/log_pip.py
@@ -0,0 +1,12 @@
+import requests
+import myutils.my_utils as my
+
+
+response = requests.get('https://api.ipify.org?format=json')
+
+myIP = response.json()['ip']
+cur_time = my.now_to_kordate()
+
+with open("publicIP.log","a",encoding="utf-8") as f:
+    f.write(f"<span class='who-time'>{cur_time}</span><span class='who-ip'>{myIP}</span>&nbsp;&nbsp; recorded on server side\n")
+         
